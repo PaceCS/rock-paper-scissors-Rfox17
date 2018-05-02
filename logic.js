675fa697a56e21bb4bc160c1407d45755c9583d2
RoryFlint <32875985+RoryFlint@users.noreply.github.com>
2018-05-02 15:01:40 -0400

    started

--- logic.js
+++ logic.js
@@ -1,34 +1,53 @@
 
 // This array will hold the options for the game
 // Fill in your options below
-const options = [/*option 1*/, /*option 2*/, /*option 3*/];
+const options = ["rock", "paper", "scissors"];
 
 // This function takes in the userChoice and then prints the results to the console.
 function playGame(userChoice) {
 
     // First remind the user of his/her choice
-    console.log(/*Put text here*/);
+    console.log("userChoice");
 
     // Now choose a random number between 0 and 2
-    const rand = /*Fill in the code for a random value between 0 and 2*/;
+    const rand = Math.floor(Math.random() *2);
 
     // Then use this random number to give the computer a choice
-    const compChoice = options[rand];
+    const compChoice = options["rock", "paper", "scissors"];
 
     // Now log the computer's choice to the console.
-    console.log(/*Put text here*/);
+    console.log("The computer chose " + compChoice  + ".");
 
-    // Now determine the winner using if/else statements
-    /*
-    |
-    |
-    This will take several if/else statements, so it will take multiple lines.
-    |
-    |
-    */
+if (compChoice === userChoice) {
+    console.log("You tied")
+}else{
+     if (compChoice == "rock") {
+         if (userChoice == "paper") {
+             console.log("You win. Paper beats rock.");
+         } else {
 
+         }
+
+    }
+
+// console.log("You win. Paper beats rock.")
+// }else {console.log("You lose. Rock beats scissors.")
+// }else if{(compChoice = "paper")
+// } if{(userChoice = "rock")
+// } {
+// console.log("You lose. Paper beats rock.")
+// } else{
+//     console.log("You win. Rock beats scissors.")
+// }
+// else
+//     if {(userChoice = "rock")
+// } {console.log("You win. Rock beats scissors.")
+// } else {
+//     console.log("You lose. Scissors beats paper.")
+// }
+
+    }
 }
-
 
 
 
