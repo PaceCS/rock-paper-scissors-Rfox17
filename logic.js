5d7e7b8f75e5749beced9ee76dfbb9e48ac68136
RoryFlint <32875985+RoryFlint@users.noreply.github.com>
2018-05-09 10:25:07 -0400

    finished rps

--- logic.js
+++ logic.js
@@ -5,32 +5,37 @@ const options = ['rock', 'paper', 'scissors'];
 // This function takes in the userChoice and then prints the results to the console.
 function playGame(userChoice) {
     // First remind the user of his/her choice
-    console.log('userChoice');
+    console.log('You chose ' + userChoice);
 
     // Now choose a random number between 0 and 2
-    const rand = Math.floor(Math.random() * 2);
+    const rand = Math.floor(Math.random() * 3);
 
     // Then use this random number to give the computer a choice
-    const compChoice = options[('rock', 'paper', 'scissors')];
+    const compChoice = options[rand];
 
     // Now log the computer's choice to the console.
+    console.log('The computer chose ' + compChoice);
 
     if (compChoice === userChoice) {
         console.log('You tied');
     } else {
         if (compChoice == 'rock') {
-            console.log('The computer chose rock.');
             if (userChoice == 'paper') {
                 console.log('You win. Paper beats rock.');
             } else {
                 console.log('You lose. Rock beats scissors.')
             }
         } else if (compChoice == 'paper') {
-            console.log('the computer chose paper');
             if (userChoice == 'scissors') {
                 console.log('You win. Scissors beats paper.');
             } else {
-
+                console.log('You lose. Paper beats rock.');
+            }
+        } else if (compChoice == 'scissors') {
+            if (userChoice == 'rock') {
+                console.log('You win. Rock beats scissors.')
+            } else {
+                console.log('You lose. Scissors beats paper.')
             }
         }
 
